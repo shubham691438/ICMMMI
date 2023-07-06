14b513422dd5277b309fea4d7eea3e6f6c54f9b9
shubham <ss691438@gmail.com>
2023-07-07 03:25:26 +0530

    made an api to send email

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,11 @@
 const express=require('express')
 const router=express.Router();
+const {sendMail}=require('../controllers/sendMailController')
 
 router.get('/',(req,res)=>{
     res.status(200).json({msg: "Welcome to API"})
   })
 
-  module.exports=router
+router.post('/send-mail',sendMail)
+
+module.exports=router
