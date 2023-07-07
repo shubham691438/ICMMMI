4075c5a41a2c21ae7dd16374c214010d9089c8af
shubham <ss691438@gmail.com>
2023-07-07 20:16:27 +0530

    changed name from userAuth to user

--- backend/api/userApi.js
+++ backend/api/userApi.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const {
     register
-}=require('../controllers/userAuthController')
+}=require('../controllers/userController')
 
 const router=express.Router()
 
