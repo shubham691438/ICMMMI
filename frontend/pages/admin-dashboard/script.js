9c70356935a3ec80d0d94bb378366e2214e150f3
shubham <ss691438@gmail.com>
2023-07-08 15:32:36 +0530

    completed admin dashboard page except for messages section and ontegrated it with the backend

--- frontend/pages/admin-dashboard/script.js
+++ frontend/pages/admin-dashboard/script.js
@@ -1,3 +1,6 @@
+
+let totalRegistrations=0,indianRegistartions=0,industries=0,academicians=0,students=0, accompanions=0,others=0;
+
 async function fetchUsers(){
     try{
         const res= await fetch("https://icmmmi-backend.onrender.com/api/user/get-all-users",{
@@ -8,14 +11,19 @@ async function fetchUsers(){
             throw new Error(users.error);
         }
 
-        let totalRegistrations=0,indianRegistartions=0,industries=0,academicians=0,students=0, accompanions=0;
+        
         users.forEach((user)=>{
             // console.log(user);
 
             totalRegistrations++;
 
             if(user.nationality.toLowerCase()=="indian" || user.nationality.toLowerCase()=="india" ) indianRegistartions++;
-            
+
+            if(user.category=="Delegates from Industry") industries++;
+            else if(user.category=="Academician") academicians++;
+            else if(user.category=="Research scholar or student") students++;
+            else if(user.category=="Accompanying persons") accompanions++;
+            else others++;
 
             const htmlMarkup=`
             <div class="card shadow mb-4">
@@ -61,6 +69,45 @@ async function fetchUsers(){
         </div>`;
             document.getElementById('people-details-body').insertAdjacentHTML('beforeend',htmlMarkup)
         })
+
+        document.getElementById('total-registartions').textContent=totalRegistrations;
+        document.getElementById('indian-registrations').textContent=indianRegistartions;
+        document.getElementById('international-registrations').textContent=totalRegistrations-indianRegistartions;
+
+        const categoryHtmlMarkup=`
+        <h4 class="small font-weight-bold">Delegates from Industries <span
+        class="float-right" ">${industries*100/totalRegistrations}%</span></h4>
+        <div class="progress mb-4">
+            <div class="progress-bar bg-danger" role="progressbar" style="width: ${industries*100/totalRegistrations}%"
+                aria-valuenow=${industries*100/totalRegistrations} aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <h4 class="small font-weight-bold">Academicians <span
+                class="float-right">${academicians*100/totalRegistrations}%</span></h4>
+        <div class="progress mb-4">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: ${academicians*100/totalRegistrations}%"
+                aria-valuenow=${academicians*100/totalRegistrations} aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <h4 class="small font-weight-bold">Research scholars & students<span
+                class="float-right">${students*100/totalRegistrations}%</span></h4>
+        <div class="progress mb-4">
+            <div class="progress-bar" role="progressbar" style="width: ${students*100/totalRegistrations}%"
+                aria-valuenow=${students*100/totalRegistrations} aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <h4 class="small font-weight-bold">Accompanying persons <span
+                class="float-right">${accompanions*100/totalRegistrations}%</span></h4>
+        <div class="progress mb-4">
+            <div class="progress-bar bg-info" role="progressbar" style="width: ${accompanions*100/totalRegistrations}%"
+                aria-valuenow=${accompanions*100/totalRegistrations} aria-valuemin="0" aria-valuemax="100"></div>
+        </div>
+        <h4 class="small font-weight-bold">Accompanying persons <span
+                class="float-right">${others*100/totalRegistrations}%</span></h4>
+        <div class="progress mb-4">
+            <div class="progress-bar bg-info" role="progressbar" style="width: ${others*100/totalRegistrations}%"
+                aria-valuenow=${others*100/totalRegistrations} aria-valuemin="0" aria-valuemax="100"></div>
+        </div>`
+
+        document.getElementById('category-progress-bar').insertAdjacentHTML('beforeend',categoryHtmlMarkup);
+        updatePieChart();
         
        
 
@@ -73,3 +120,40 @@ async function fetchUsers(){
 }
 
 fetchUsers();
+
+
+// Pie Chart Example
+
+function updatePieChart(){
+    var ctx = document.getElementById("myPieChart");
+    console.log(ctx);
+    var myPieChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ["Indian", "International"],
+        datasets: [{
+        data: [indianRegistartions,totalRegistrations-indianRegistartions],
+        backgroundColor: ['#4e73df', '#1cc88a'],
+        hoverBackgroundColor: ['#2e59d9', '#17a673'],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+        }],
+    },
+    options: {
+        maintainAspectRatio: false,
+        tooltips: {
+        backgroundColor: "rgb(255,255,255)",
+        bodyFontColor: "#858796",
+        borderColor: '#dddfeb',
+        borderWidth: 1,
+        xPadding: 15,
+        yPadding: 15,
+        displayColors: false,
+        caretPadding: 10,
+        },
+        legend: {
+        display: false
+        },
+        cutoutPercentage: 0,
+    },
+    });
+}
