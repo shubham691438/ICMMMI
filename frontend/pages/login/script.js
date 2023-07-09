088eaa18425f2ef48820cb18fa0b176163af290c
shubham <ss691438@gmail.com>
2023-07-09 19:00:38 +0530

    minor fix

--- frontend/pages/login/script.js
+++ frontend/pages/login/script.js
@@ -103,7 +103,7 @@ userLoginForm.addEventListener('submit',function(event){
             // console.log(json);
             localStorage.setItem('user',JSON.stringify(json));
 
-            window.location.href = "../profile/index.js";
+            window.location.href = "../profile/index.html";
             
           }  
           
