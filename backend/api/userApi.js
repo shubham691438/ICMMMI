b321789d027b153f328288dd8fe6f2c607c3f9b1
shubham <ss691438@gmail.com>
2023-07-10 20:09:34 +0530

    frontend+backend for profile page is done

--- backend/api/userApi.js
+++ backend/api/userApi.js
@@ -2,7 +2,10 @@ const express=require('express')
 const {
     login,
     register,
+    getUser,
+    updateUser
 }=require('../controllers/userController')
+const requireAuth=require('../middleware/requireAuth')
 const router=express.Router()
 
 //register post request to create a new user in db
@@ -10,6 +13,12 @@ router.post('/register',register)
 
 // login post request to authenticate a current user
 router.post('/login',login)
+
+//authenticated request to get user
+router.get('/:id',requireAuth,getUser)
+
+//authenticated request to update user
+router.put('/update/:id',requireAuth,updateUser)
 
 
 
